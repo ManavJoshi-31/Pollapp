18f6fe8a28b15c36146ed4c449d4c8399650b51e
Manav <manavjoshi313@gmail.com>
2026-01-19 12:31:55 +0530

    [UPDATE] views.py added shortcut

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,10 +1,8 @@
 from django.http import Http404
 from django.http import HttpResponse
-
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
-
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -13,11 +11,9 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
